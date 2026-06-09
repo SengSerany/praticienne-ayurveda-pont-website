eb57a1d4cf5fa4bede8780d1f86c0a89a0289cdc
Developer <cranyseng@gmail.com>
2026-06-09 14:09:27 +0200

    add cloudflare adapter and runtime env typing

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="astro/client" />
+
+type CloudflareRuntime = import('@astrojs/cloudflare').Runtime<Env>;
+
+interface Env {
+  BREVO_API_KEY?: string;
+  BREVO_LIST_LETTRE_ID?: string;
+  BREVO_NOTIF_EMAIL?: string;
+  BREVO_DOI_TEMPLATE_ID?: string;
+  BREVO_DOI_REDIRECT_URL?: string;
+}
+
+declare namespace App {
+  interface Locals extends CloudflareRuntime {}
+}
